d539b5dfd70ff78101298106b9675b1f1412159b
emptyrivers <afaure6@gmail.com>
2017-12-15 15:27:04 -0600

    basic functions required to create and maintain chains

--- modules/Chain.lua
+++ modules/Chain.lua
@@ -1,66 +1,48 @@
 require "util"
---local Link = require "modules.Link"
 local Chain = {}
 local chainMt = { __index = Chain }
+local btest= bit32.btest
+--[[
+A Chain is an object, which represents a somewhat idealized factory:
+* The inflow field is an array of {item, amount}, representing the items fed into the factory. `item` is a string that refers to a node from the parent graph
+* The outflow represents items which are leaving the factory. Structure is {item, amount, intended}, where `intended` is a tristate.
+  True if intended, False if a byproduct, nil if unknown/don't care.
+* The sideflow field represents items flowing between branches of the factory.
+  Structure is {item, amount, Sidechain, index,}, where SideChain is a Chain object,
+  and index is the point of insertion/extraction (negative if leaving this factory, positive if entering)
+* Each link represents a group of machines, all of which are performing a single recipe.
+  Each link is itself a chain object, and there can be several types:
+    if type == -1, then chain is a single recipe (and #links == 0). Inflow travels directly to outflow.
+    else, type is considered a bitfield:
+    0x1 = compound (at least one of the links is a chain of type >= 0)
+    0x2 = cyclic (at least one of the ingredients is also a product (there's a sense of "self-sideflow")
+    0x4 = branching (at least one of the byproducts is a chain)
+--]]
 
--- type is a bitfield: 
--- -1 (special!) chain is a single recipe
--- 1 = compound (at least one of the links is a chain with length > 1) 
--- 2 = cyclic (at least one of the ingredients is also a product
--- 4 = branching (at least one of the byproducts is a chain)
+function Chain:New(graph) -- where data is an edge in the chain's parent graph
+  local chain = setmetatable(
+  {
+    links = {},
+    inflow = {},
+    outflow = {},
+    sideflow = {},
+    parent = graph,
+    type = -1,
+  }, chainMt)
+end
 
-function Chain:New(data)
-  if data then
-    local chain = setmetatable({
-      type = 0,
-      links = {table.deepcopy(data))
-      ingredients = {}
-      products = {}
-      byproducts = {}
-   }, chaimMt) 
-    chain.ingredients = table.deepcopy(chain.links[1].ingredients)
-    chain.products = table.deepcopy(chain.links[1].products)  
-  else
-    return setmetatable({
-      type = 0,
-      links = {},
-      ingredients = {},
-      byproducts = {},
-      products = {},
-    }, chainMt)
+
+
+function Chain.setmetatables(chain)
+  for _, link in pairs(chain.links) do
+    Chain.setmetatables(link)
   end
-end
-
-function Chain:Evaluate(input, isSubCall)
-  local result = input and table.deepcopy(input) or table.deepcopy(self.ingredients)
-  for _, link in ipairs(self.links) do
-    if link.type == -1 then
-      local bottleNeck, bottleNeckAmount = "null", math.huge
-        for ingredient, amount in pairs(link.ingredients) do
-          if not result[ingredient] or result[ingredient] == 0 then
-            bottleNeck, bottleNeckAmount = ingredient, 0
-            break
-          elseif result[ingredient] < bottleNeckAmount then
-            bottleNeck, bottleNeckAmount = ingredient, result[ingredient]
-            
-end
-
-function Chain:Append(toAppend, isBegin)
-  if isBegin then
-    
-  else
-    table.insert(self.links, toAppend)
-    
+  for _, byproduct in pairs(chain.sideflow) do
+    Chain.setmetatables(byproduct[3])
   end
-  return self
+  return setmetatable(chain, chainMt)
 end
 
-function Chain:Join(toJoin, atByproduct)
-  local branchPoint = self.byproducts[atByproduct]
-  if branchPoint then 
-    
-  else
-    error( "ProductionChain: Attempt to join at a non-existent branch point.", 2 )
-  end
-  return self
-end
+
+
+return Chain
