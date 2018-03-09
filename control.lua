b43c4b8df11a67bd015cb26ca97a6e45a8567e39
emptyrivers <afaure6@gmail.com>
2018-03-09 05:31:16 -0600

    switch references to lib

--- control.lua
+++ control.lua
@@ -1,28 +1,23 @@
+
+
 for k, v in pairs(defines.events) do
   _G[k] = v -- because fuck writing defines.events.on_whatever_event every damn time!
 end
+
+
 -- modules
-local modules = {
-  HyperGraph   = require "modules.HyperGraph",
-  PocketWatch  = require "modules.PocketWatch",
-  GUI          = require "modules.GUI",
-  LPSolve      = require "modules.LPSolve",
-  widgets      = require "modules.widgets",
-}
-local snippets = require "modules.snippets"
-local logger   = require "modules.logger"
+local lib = require "lib"
+local snippets = require "misc.snippets"
+local logger   = require "misc.logger"
 local inspect  = require "inspect"
+
 -- upvalues
-local taskMap     = modules.PocketWatch.taskMap
-local responses   = modules.GUI.responses
+local taskMap     = lib.PocketWatch.taskMap
+local responses   = lib.GUI.responses
 local snippets    = snippets
 
 -- upvalues to be assigned in init/load
 local timers, fullGraph, techTree, forceGraphs, models
-
--- 
-
-
 
 -- taskMap function definitions
 do
@@ -154,7 +149,7 @@ do
           end
           data.products[name] = product.amount or (product.probability * .5 * (product.amount_min + product.amount_max))
         end
-        --[[ for id, amount in pairs(data.ingredients) do
+        for id, amount in pairs(data.ingredients) do
           if data.products[id] then
             data.catalysts = data.catalysts or {}
             local diff = data.products[id] - amount
@@ -169,7 +164,7 @@ do
             end
           end
         end
-         ]]graph:AddEdge(data)
+        graph:AddEdge(data)
       end
     end
     return 
@@ -180,79 +175,76 @@ do
 end
 
 -- script handlers
-  script.on_init(function()
-    -- create persistent values
-    for _, module in pairs(modules) do
-      if module.Init then module:Init() end
+script.on_init(function()
+  lib:Init()
+  fullGraph,        forceGraphs,        timers,        models,        techTree =
+  global.fullGraph, global.forceGraphs, global.timers, global.models
+  timer = lib.PocketWatch:New('main')
+  global.techTree = timers.main:Do("buildTechTree")
+  techTree = global.techTree
+  timers.main:Do("explore",fullGraph)
+  commands.add_command("pc","test",function() game.print("Hello, World!")end)
+end)
+
+script.on_load(function()
+  lib:OnLoad()
+  fullGraph,        forceGraphs,        timers,        models,        techTree =
+  global.fullGraph, global.forceGraphs, global.timers, global.models, global.techTree
+  for _, timer in pairs(timers) do
+    if timer.working then
+      script.on_event(on_tick, timer.continueWork)
+      break
     end
-    fullGraph,        forceGraphs,        timers,        models,        techTree =
-    global.fullGraph, global.forceGraphs, global.timers, global.models
-    timer = modules.PocketWatch:New('main')
-    global.techTree = timers.main:Do("buildTechTree")
-    techTree = global.techTree
-    timers.main:Do("explore",fullGraph)
-    commands.add_command("pc","test",function() game.print("Hello, World!")end)
-  end)
+  end
+  commands.add_command("pc","test",function() game.print("Hello, World!")end)
+end)
 
-  script.on_load(function()
-    for _, module in pairs(modules) do if module.Load then module:Load() end end
-    fullGraph,        forceGraphs,        timers,        models,        techTree =
-    global.fullGraph, global.forceGraphs, global.timers, global.models, global.techTree
-    for _, timer in pairs(timers) do
-      if timer.working then
-        script.on_event(on_tick, timer.continueWork)
-        break
-      end
-    end
-    commands.add_command("pc","test",function() game.print("Hello, World!")end)
-  end)
+script.on_configuration_changed(function(event)
+  lib:OnConfigurationChanged()
+  global.techTree = timer:Do("buildTechTree")
+  timer:Do("explore", fullGraph, nil, true)
+  for force, graph in pairs(global.forceGraphs) do
+    timer:Do("explore", graph, force, true)
+  end
+end)
 
-  script.on_configuration_changed(function(event)
-    for _, module in pairs(modules) do if module.OnConfigurationChanged then module:OnConfigurationChanged() end end
-    global.techTree = timer:Do("buildTechTree")
-    timer:Do("explore", fullGraph, nil, true)
-    for force, graph in pairs(global.forceGraphs) do
-      timer:Do("explore", graph, force, true)
-    end
-  end)
+script.on_event(on_research_finished, function(event)
+  local unlockedRecipes = techTree[event.research.name] and techTree[event.research.name].unlocks
+  local graph =  forceGraphs[event.research.force.name]
+  if unlockedRecipes and graph then
+    timer:Do('updatePaths', graph, unlockedRecipes)
+  end
+end)
 
-  script.on_event(on_research_finished, function(event)
-    local unlockedRecipes = techTree[event.research.name] and techTree[event.research.name].unlocks
-    local graph =  forceGraphs[event.research.force.name]
-    if unlockedRecipes and graph then
-      timer:Do('updatePaths', graph, unlockedRecipes)
-    end
-  end)
+script.on_event(on_player_changed_force, function(event)
+  local playerID = event.player_index
+  local playerForce = game.players[playerID].force.name
+  if not forceGraphs[playerForce] then
+    forceGraphs[playerForce] = lib.HyperGraph:New()
+    timer:Do('explore', forceGraphs[playerForce], playerForce)
+  end
+end)
 
-  script.on_event(on_player_changed_force, function(event)
-    local playerID = event.player_index
-    local playerForce = game.players[playerID].force.name
-    if not forceGraphs[playerForce] then
-      forceGraphs[playerForce] = modules.HyperGraph:New()
-      timer:Do('explore', forceGraphs[playerForce], playerForce)
-    end
-  end)
+script.on_event(on_player_created, function(event)
+  local playermodel = lib.GUI:New(event.player_index)
+  playermodel.top:Add(modules.widgets.Top_Button)
+  logger:log(1,'file',{filePath = "GUI_Log",data = playermodel:Dump(), for_player = event.player_index})
+  local playerForce = game.players[event.player_index].force.name
+  if not forceGraphs[playerForce] then
+    forceGraphs[playerForce] = lib.HyperGraph:New()
+    timer:Do('explore', forceGraphs[playerForce], playerForce)
+    end  
+    logger:log(1,'file',{filePath = "Graph_Log",data = fullGraph:Dump(), for_player = event.player_index})
+end)
 
-  script.on_event(on_player_created, function(event)
-    local playermodel = modules.GUI:New(event.player_index)
-    playermodel.top:Add(modules.widgets.Top_Button)
-    logger:log(1,'file',{filePath = "GUI_Log",data = playermodel:Dump(), for_player = event.player_index})
-    local playerForce = game.players[event.player_index].force.name
-    if not forceGraphs[playerForce] then
-      forceGraphs[playerForce] = modules.HyperGraph:New()
-      timer:Do('explore', forceGraphs[playerForce], playerForce)
-     end  
-     logger:log(1,'file',{filePath = "Graph_Log",data = fullGraph:Dump(), for_player = event.player_index})
-  end)
+script.on_event(on_player_removed, function(event)
+  lib.GUI:Delete(event.player_index)
+end)
 
-  script.on_event(on_player_removed, function(event)
-    modules.GUI:Delete(event.player_index)
-  end)
-
-  script.on_event(on_forces_merging, function(event)
-      forceGraphs[event.force.name] = nil
-    end
-  )
+script.on_event(on_forces_merging, function(event)
+    forceGraphs[event.force.name] = nil
+  end
+)
 
 
 
