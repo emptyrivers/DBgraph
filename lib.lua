b301bce989d9488c43038df23c198fc028a9d755
emptyrivers <afaure6@gmail.com>
2018-03-18 11:53:53 -0500

    repair metatables recursively may do this in a different way some other time, but this works nicely

--- lib.lua
+++ lib.lua
@@ -1,24 +1,48 @@
 
-
 local libs = {
+    rational = require "libs.rational",
+    vector = require "libs.vector",
+    matrix = require "libs.matrix",
     HyperGraph = require "libs.HyperGraph",
     PocketWatch = require "libs.PocketWatch",
     GUI = require "libs.GUI",
-    vector = require "libs.vector",
-    matrix = require "libs.matrix",
-    rational = require "libs.rational"
 }
 
 local lib = {}
-
+function repairMetatables(broken) --should do this smarter, not harder
+    local seen = {}
+    local function _recurse(t)
+        if type(t) == "table" then 
+            if type(t.__self) ~= 'userdata' then -- no touching userdata
+                local mt = t.type and libs[t.type] and libs[t.type].mt
+                if mt then
+                    setmetatable(t,mt)
+                end
+                for k,v in pairs(t) do
+                    if type(k) == "table" and not seen[k] then
+                        seen[k] = true
+                        _recurse(k)
+                    end
+                    if type(v) == "table" and not seen[v] then
+                        seen[v] = true
+                        _recurse(v)
+                    end
+                end
+            end
+        end
+    end
+    _recurse(broken)
+end
 function lib:Init()
-    for _, v in pairs(libs) do
+    for k, v in pairs(libs) do
+        log('initializing '..k)
         if v.Init then
             v:Init()
         end
     end
 end
 function lib:Load()
+    repairMetatables(global)
     for _, v in pairs(libs) do
         if v.Load then
             v:Load()
