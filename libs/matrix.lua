bebcb7ff06ffb8597a0bfacc6004ddd248c2a0e2
emptyrivers <afaure6@gmail.com>
2018-03-18 11:55:54 -0500

    don't try to repair metatables here. Also, allow certain methods to be called without being saved to global (since they are closures which can't be saved)

--- libs/matrix.lua
+++ libs/matrix.lua
@@ -34,22 +34,9 @@ local rational = require "libs.rational"
 local inspect = require "inspect"
 local matrix = {}
 local matrixMt = {}
-local weakMt = {__mode = "kv"}
 local prototype = {type = "matrix"}
+matrix.mt = matrixMt
 
--- **RIVERS** addition for factorio
-
-function matrix:Init()
-    global.matrices = setmetatable({}, weakMt)
-    global.matrixCount = 0
-end
-
-function matrix:Load()
-    for _, matrix in pairs(global.matrices) do
-        setmetatable(matrix, matrixMt)
-    end
-    setmetatable(global.matrices, weakMt)
-end
 ----
 ---- constructors
 ----
@@ -70,8 +57,6 @@ function matrix.new(rows, columns)
    end
    M.vectors = vectors
    setmetatable(M, matrixMt)
-   global.matrices[global.matrixCount] = M
-   global.matrixCount = global.matrixCount + 1
    return M
 end
 
@@ -102,7 +87,7 @@ setmetatable(matrix, {__call=__call})
 ----
 
 local function __index(self, i)
-   return self.vectors[i]
+   return self.vectors[i] or matrix[i]
 end
 matrixMt.__index = __index
 
@@ -252,7 +237,7 @@ local function __tostring(self)
    local max = 0
    local digits = 4 -- how many digits to print for each entry
    local padding = 2 -- how much padding between entries
-   if rows <= 100 and columns <= 100 then
+   if rows <= 100 and columns <= 100 or self:nonzero() < 20 then
       for i = 1, rows do
          s[i] = {}
          for j = 1, columns do
@@ -274,15 +259,12 @@ local function __tostring(self)
    end
    return s
 end
-   
 matrixMt.__tostring = __tostring
 
 matrixMt.__concat = function(a,b)
     return tostring(a)..tostring(b)
 end
-----
----- prototype methods
-----
+
 
 local function copy(self)
    local M = matrix.new(self.rows, self.columns)
@@ -291,7 +273,7 @@ local function copy(self)
    end
    return M
 end
-prototype.copy = copy
+matrix.copy = copy
 
 local function size(self)
    return self.rows, self.columns
@@ -302,17 +284,6 @@ local function vects(self)
    return pairs(self.vectors)
 end
 prototype.vects = vects
-
-local function map(self, fn)
-   local M = matrix.new(self.rows, self,columns)
-   for i, v in self:vects() do
-      for j, e in v:elts() do
-         M[i][j] = fn(e, i, j)
-      end
-   end
-   return M
-end
-prototype.map = map
 
 
 -- Count the number of nonzero elements in a matrix.
@@ -339,266 +310,7 @@ local function transpose(self)
    end
    return M
 end
-prototype.transpose = transpose
-prototype.t = transpose
-
-
--- Find the inverse of a matrix
-local function inverse(self)
-   local columns = self.columns
-   local M = matrix.id(columns)
-   for i = 1, columns do
-      M[i] = matrix.gepp(self, M[i])
-   end
-   return M:t()
-end
-prototype.inverse = inverse
-
-
--- Returns with the value of the largest element in self.
-local function max(self)
-   local max = self[1]:max()
-   for i, v in self:vects() do
-      local m = v:max()
-      if m > max then max = m end
-   end
-   return max
-end
-prototype.max = max
-
-
--- Returns with the value of the smallest element in self
-local function min(self)
-   local min = self[1]:min()
-   for i, v in self:vects() do
-      local m = v:min()
-      if m < min then min = m end
-   end
-   return min
-end
-prototype.min = min
-
- 
-----
----- decompositions
-----
-
--- LU decompose A into permutation matrix P, lower triangle L and
--- upper triangle P.
-function matrix.lu(A)
-   local rows, columns = A.rows, A.columns
-   assert(rows == columns, 
-          "only LU decomposing square matrices is supported")
-   assert(A:nonzero() > 0, "can't LU decompose the zero matrix")
-   -- Don't want to work with A directly, or its elements will be
-   -- changed, and that would be bad, so make a copy of A and use that.
-   local A = A:copy()
-   local lambda = vector.new(rows)
-   for i = 1, rows do
-      lambda[i] = i
-   end
-   for k = 1, rows - 1 do
-      local Amax = 0
-      local imax = 0
-      -- find the pivot position for the current column
-      for i = k, rows do
-         local temp = math.abs(A[i][k])
-         if temp > Amax then
-            Amax, imax = temp, i
-         end
-      end
-      -- move the pivot position to the top row of the submatrix being
-      -- worked on
-      A[k], A[imax] = A[imax], A[k]
-      -- swap entries in lambda so we remember how we permuted the rows
-      lambda[k], lambda[imax] = lambda[imax], lambda[k]
-      -- calculate the multipliers, and record them in A
-      local Akk = A[k][k]
-      for i = k + 1, rows do
-         A[i][k] = A[i][k] / Akk
-      end
-      -- update A
-      for j = k + 1, rows do
-         for i = k + 1, rows do
-            A[i][j] = A[i][j] - A[i][k] * A[k][j]
-         end
-      end
-   end
-   local P = matrix.new(rows, rows)
-   -- create the permutation matrix P
-   for i = 1, rows do
-      P[lambda[i]][i] = 1
-   end
-   -- create L and turn A into U by zeroing out all the entries of L in A
-   local L = matrix.id(rows)
-   for i = 1, rows do
-      for j = 1, i - 1 do
-         L[i][j], A[i][j] = A[i][j], rational.zero
-      end
-   end
-   return P, L, A
-end
-
-
--- Find the Cholesky decomposition of A using inner products
-function matrix.cholesky(A)
-   local rows, columns = A.rows, A.columns
-   assert(rows == columns, "Matrix must be square")
-   local A = A:copy()
-   for i = 1, rows do
-      for k = 1, i -1 do
-         A[i][i] = A[i][i] - (A[k][i] * A[k][i])
-      end
-      if A[i][i] <= 0 then 
-         error("matrix is not symmetric positive definite")
-      end
-      A[i][i] = math.sqrt(A[i][i])
-      for j = i + 1, rows do
-         for k = 1, i - 1 do
-            A[i][j] = A[i][j] - A[k][i] * A[k][j]
-         end
-         A[i][j] = A[i][j] / A[i][i]
-      end
-   end
-   -- remove entries below the diagonal
-   for i = 2, rows do
-      for j = 1, i - 1 do
-         A[i][j] = rational.zero
-      end
-   end
-   return A
-end
-
-
--- Givens rotation; used for QR factorization.
--- The way r, c and s is calculated here is not ideal. 
---
--- See: 
--- Anderson, Edward (2000), Discontinuous Plane Rotations and the
---   Symmetric Eigenvalue Problem. LAPACK Working Note 150, University
---   of Tennessee, UT-CS-00-454, December 4, 2000.
---   http://www.netlib.org/lapack/lawnspdf/lawn150.pdf
-local function givens(A, i, j)
-   local G = matrix.id(A.rows)
-   local a = A[i][i]
-   local b = A[j][i]
-   local r = math.sqrt(a^2 + b^2)
-   local c = a / r
-   local s = b / r
-   G[i][i] = c
-   G[j][j] = c
-   G[i][j] = s
-   G[j][i] = -s
-   return G
-end
-
-
--- Find the QR decomposition of matrix A using Givens rotations.
--- Returns the orthogonal matrix Q and the upper triangular matrix R.
--- Q * R = A (not accounting for error).
-function matrix.qr(A)
-   local A = A:copy()
-   local Q = matrix.id(A.rows)
-   for j = 1, A.columns do
-      for i = A.rows, j + 1, -1 do
-         local G = givens(A, j, i)
-         A = G * A
-         A[i][j] = 0
-         Q = G * Q
-      end
-   end
-   return Q:t(), A
-end
-
-
-
-----
----- solvers (direct, for iterative, see isolv.lua)
-----
-
--- solve Ax = b using gaussian elimination with parital pivoting
--- returns x, the vector of solutions
-function matrix.gepp(A, b)
-    assert(A:nonzero() > 0, "cannot % the zero matrix")
-   log('solving Ax = b, A=\n'..tostring(A)..'\nb='..tostring(b))
-   local rows, columns = A.rows, A.columns
-   -- make a copy of A, otherwise the algorithm will modify the A that was 
-   -- passed in.
-   local A = A + matrix.new(rows, columns)
-   local lambda = vector.new(rows)
-   for i = 1, rows do
-      lambda[i] = i
-   end
-   for k = 1, rows - 1 do
-      local Amax = 0
-      local imax = 0
-      -- find column pivot positions
-      for i = k, rows do
-         local temp = rational.abs(A[i][k])
-         if temp > Amax then
-            Amax = temp
-            imax = i
-         end
-      end    
-      if imax ~= k then
-         -- swap rows of A
-         log('swapping row '..k..' with row '..imax)
-         A[k], A[imax] = A[imax], A[k]
-         -- swap entries in lambda to remember pivots
-         lambda[k], lambda[imax] = lambda[imax], lambda[k]
-         -- calculate the multipliers
-      end
-      local A_kk = A[k][k]
-      for i = k + 1, rows do
-         log("A_"..i.."_"..k.." = ".."A_"..i.."_"..k.." / A_"..k.."_"..k.."="..(A[i][k] / A_kk))
-         A[i][k] = A[i][k] / A_kk
-      end
-      -- update A
-      for j = k + 1, rows do
-         for i = k + 1, rows do
-            A[i][j] = A[i][j] - A[i][k] * A[k][j]
-         end
-      end
-   end
-   -- Permute the right hand side as indicated by lambda
-   local bh = vector.new(rows)
-   for i = 1, rows do 
-      bh[i] = b[lambda[i]];
-   end
-   -- Solve the lower triangular system Lc = b
-   local c = vector.new(rows)
-   for i = 1, rows do
-      local sum = bh[i]
-      for j = 1, i - 1 do
-         sum = sum - A[i][j] * c[j]
-      end
-      c[i] = sum
-   end
-   -- Solve the upper triangular system, Uz = c
-   local z = vector.new(rows);
-   for i = rows, 1, -1 do
-      local sum = c[i]
-      for j = i + 1, rows do
-         sum = sum - A[i][j] * z[j]
-      end
-      if A[i][i] == 0 then
-         error("the algorithm fails")
-      else
-         z[i] = sum / A[i][i]
-      end
-   end
-   -- permute the z vector to get the solution x (this
-   -- step is required if there were any column switches
-   local x = vector.new(rows)
-   for i = 1, rows do
-      x[i] = z[i]
-   end
-   return x
-end
--- Use the % operator as shorthand for gepp.
--- i.e. x = A % b to get the solutions of Ax = b
-matrixMt.__mod = matrix.gepp
-
+matrix.t = transpose
 
 ----
 ---- miscellaneous
@@ -611,6 +323,12 @@ function matrix.id(n)
       M[i][i] = rational.one
    end
    return M
+end
+
+function matrix.join(M, i, j, r)
+    -- returns a matrix with 1 fewer row, such that M_new[i] = M_old[i] + r * M[j], and replace M[j] with zeros
+    M[i], M[j] = M[i] + r * M[j], vector.new(M.rows)
+    return M
 end
 
 return matrix
